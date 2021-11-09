b0fbf3212730e14c0b0243b8f657e82fe26ab015
AlexiVides <88692360+AlexiVides@users.noreply.github.com>
2021-11-08 20:09:05 -0600

    Cajero Automatico MVC

---
+++ CajeroAutomatico/CajeroAutomatico/Controllers/CajeroController.cs
@@ -0,0 +1,50 @@
+﻿using CajeroAutomatico.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CajeroAutomatico.Controllers
+{
+    public class CajeroController : Controller
+    {
+        // GET: Cajero
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index1()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index1(Cajero c)
+        {
+            if(c.monto % 5 == 0)
+            {
+                return Redirect("Correcto");
+            }
+            else
+            {
+                return Redirect("Error");
+            }
+            
+        }
+
+        [HttpGet]
+        public ActionResult Correcto()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
